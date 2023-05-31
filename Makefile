bb60574e249debc4f6d463572673fd28e0ad318b
Carlos Gomez <carliros.g@gmail.com>
2023-05-31 16:09:40 -0400

    add tailwind WIP

---
+++ Makefile
@@ -0,0 +1,11 @@
+
+build:
+	spago build
+	npm install
+	npm run tw:build
+	spago bundle-app -t ./docs/assets/js/app/app.js
+
+watch:
+	spago build
+	npm run tw:build
+	spago bundle-app --watch -t ./docs/assets/js/app/app.js
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    './src/**/*.{purs}',
+    './docs/index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
